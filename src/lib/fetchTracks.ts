098845b44390b401cb9fd5122e1049878cf9855d
Lozink <117382724+Lozink@users.noreply.github.com>
2023-05-31 19:21:12 +0200

    1 file modified

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,7 +1,7 @@
 const apiToken = '';
 
 export const fetchTracks = async () => {
-  const apiToken = "https://api.spotify.com/v1/me/shows?offset=0&limit=20"
+  const apiToken = 'BQCheKvAxXwNPkmMsF_k1xppV1oFwCjuc48sEuK9dsBCny3HrQxZ8J3vWp_UuLks0VO2kVftK3XqHLgl3RXtUaXlTi-06hyMoeXlqT6Eag8rhjN1BSjQUdYcb_LbwLnojlfpw_79dxxvl_71H3krl8Me0GYqXEonRrRhShncp3GiPkWF0jvI_gYiBjIFa0F2pieuWN-o8XDEXSna0A'
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
     method: 'GET',
     headers: {
